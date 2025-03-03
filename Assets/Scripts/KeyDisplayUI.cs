157081c648f4839b091bbe7ad5b32649486cba82
jonghyun109 <mimj2527@gmail.com>
2025-03-03 18:19:55 +0900

    [feat] 버튼 UI 추가

---
+++ Assets/Scripts/KeyDisplayUI.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class KeyDisplayUI : MonoBehaviour
+{
+    public Button wKey, aKey, sKey, dKey, ctrlKey, shiftKey; //** UI 버튼들
+    private Dictionary<KeyCode, Button> keyMappings; //** 키와 버튼 매핑
+
+    private Color defaultColor = Color.gray; //** 기본 색상
+    private Color pressedColor = Color.blue; //** 눌렀을 때 색상
+
+    void Start()
+    {
+        //** 키와 UI 버튼 매핑
+        keyMappings = new Dictionary<KeyCode, Button>
+        {
+            { KeyCode.W, wKey },
+            { KeyCode.A, aKey },
+            { KeyCode.S, sKey },
+            { KeyCode.D, dKey },
+            { KeyCode.LeftControl, ctrlKey },
+            { KeyCode.LeftShift, shiftKey }
+        };
+
+        foreach (var button in keyMappings.Values)
+        {
+            SetButtonColor(button, defaultColor);
+        }
+    }
+
+    void Update()
+    {
+        foreach (var key in keyMappings.Keys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                SetButtonColor(keyMappings[key], pressedColor); //** 키를 누르면 색 변경
+            }
+            if (Input.GetKeyUp(key))
+            {
+                SetButtonColor(keyMappings[key], defaultColor); //** 키를 떼면 원래 색으로 복귀
+            }
+        }
+    }
+
+    void SetButtonColor(Button button, Color color)
+    {
+        ColorBlock cb = button.colors;
+        cb.normalColor = color;
+        cb.highlightedColor = color;
+        cb.pressedColor = color;
+        button.colors = cb;
+    }
+}
